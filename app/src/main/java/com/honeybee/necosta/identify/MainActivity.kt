a4c6770de655770718c499f4b0367e3cfa6979c6
Nelson Costa <nelsonc@Nelsons-MBP.home>
2018-08-02 13:55:15 +0100

    Added logic to change between layouts

--- app/src/main/java/com/honeybee/necosta/identify/MainActivity.kt
+++ app/src/main/java/com/honeybee/necosta/identify/MainActivity.kt
@@ -2,11 +2,16 @@ package com.honeybee.necosta.identify
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun onButtonStartClick(v: View) {
+        setContentView(R.layout.activity_video)
     }
 }
